34a2646c8c8855df07318c822997476f7258d42a
ChuckLu <244657538@qq.com>
2015-03-14 17:46:42 +0800

    1.在QuickStart下新建了2-Medium文件夹，并将MultipleCommandAssembly文件夹移动到其下。 2.新建了测试SampleServer的Test项目

---
+++ QuickStart/2-Medium/MultipleCommandAssembly/Test/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SuperSocket.QuickStart.SampleServer;
+using SuperSocket.SocketEngine;
+using SuperSocket.SocketBase;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bootstrap = BootstrapFactory.CreateBootstrap();
+            bool flag = bootstrap.Initialize();
+            if (flag == false)
+            {
+                Console.WriteLine("初始化失败");
+                return;
+            }
+
+            StartResult result = bootstrap.Start();
+            Console.WriteLine(string.Format("Start Result:{0}", result));
+            if (result == StartResult.Failed)
+            {
+                Console.WriteLine("Failed to start!");
+                return;
+            }
+            Console.WriteLine("Press key 'q' to stop it!");
+
+            while (Console.ReadKey().KeyChar != 'q')
+            {
+                System.Threading.Thread.Sleep(1000);
+                continue;
+            }
+
+            Console.WriteLine();
+
+            bootstrap.Stop();
+
+            Console.WriteLine("The server was stopped!");
+            Console.ReadKey();
+        }
+    }
+}
