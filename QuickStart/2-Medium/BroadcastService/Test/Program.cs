aa83822135e949bb3e92e9e10617ab065b12d87e
ChuckLu <244657538@qq.com>
2015-03-15 23:52:56 +0800

    1.移动【2-Medium】下的BroadService的位置，同时添加对应的测试项目

---
+++ QuickStart/2-Medium/BroadcastService/Test/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SuperSocket.SocketBase;
+using SuperSocket.SocketEngine;
+using SuperSocket.QuickStart.BroadcastService;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bootstrap = BootstrapFactory.CreateBootstrap();
+            bool flag = bootstrap.Initialize();
+            if (flag == false)
+            {
+                Console.WriteLine("初始化配置失败");
+                return;
+            }
+
+            StartResult result = bootstrap.Start();
+            Console.WriteLine(string.Format("Start Result:{0}", result));
+            if (result == StartResult.Failed)
+            {
+                Console.WriteLine("Failed to start!");
+                return;
+            }
+            Console.ReadKey();
+        }
+    }
+}
